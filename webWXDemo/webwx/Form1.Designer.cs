5edaf5fd185f0f70a4a6ab8b60a38f096443397c
tanxiaoming <tanxiaoming@youanmi.com>
2017-05-08 18:41:42 +0800

    web版微信调用demo

---
+++ webWXDemo/webwx/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace webwx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nickname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.wxid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.fromUserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toUserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.content = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(276, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 183);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nickname,
+            this.wxid});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(267, 622);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // nickname
+            // 
+            this.nickname.HeaderText = "昵称";
+            this.nickname.Name = "nickname";
+            this.nickname.ReadOnly = true;
+            // 
+            // wxid
+            // 
+            this.wxid.HeaderText = "微信id";
+            this.wxid.Name = "wxid";
+            this.wxid.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(276, 603);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(500, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(782, 603);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.fromUserName,
+            this.toUserName,
+            this.time,
+            this.content});
+            this.dataGridView2.Location = new System.Drawing.Point(276, 28);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(592, 569);
+            this.dataGridView2.TabIndex = 5;
+            this.dataGridView2.Visible = false;
+            // 
+            // fromUserName
+            // 
+            this.fromUserName.HeaderText = "发送者";
+            this.fromUserName.Name = "fromUserName";
+            this.fromUserName.Width = 80;
+            // 
+            // toUserName
+            // 
+            this.toUserName.HeaderText = "发送给";
+            this.toUserName.Name = "toUserName";
+            this.toUserName.Width = 80;
+            // 
+            // time
+            // 
+            this.time.HeaderText = "时间";
+            this.time.Name = "time";
+            this.time.Width = 80;
+            // 
+            // content
+            // 
+            this.content.HeaderText = "内容";
+            this.content.Name = "content";
+            this.content.Width = 300;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 625);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nickname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn wxid;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fromUserName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn toUserName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn content;
+    }
+}
+
---
+++ webWXDemo/webwx/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace webwx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.Threading.Thread thread = new System.Threading.Thread(start);
+            thread.Start();
+        }
+
+        public void on_loadQrEventHandler(Bitmap bmp)
+        {
+            pictureBox1.BackgroundImage = bmp;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private WebMMengine.WebMMengine web;
+        private void start()
+        {
+            web = new WebMMengine.WebMMengine();
+            web.on_loadQr += on_loadQrEventHandler;
+            web.on_scanQR += Web_on_scanQR;
+            web.on_scanQR_conform += Web_on_scanQR_conform;
+            web.on_GetContactList += Web_on_GetContactList;
+            web.on_NewMessage += Web_on_NewMessage;
+            web.WebMM_Start();
+        }
+
+        public System.Collections.ArrayList Contacts ;
+
+        private void Web_on_NewMessage(WebMMengine.webmm_mesg msg)
+        {
+            foreach(WebMMengine.Contact contact in Contacts)
+            {
+                if(msg.FromUserName == contact.UserName)
+                {
+                    dataGridView2.BeginInvoke(new MethodInvoker(() =>
+                    {
+                        addMsg(contact.NickName, null, msg.Content);
+                    }));
+                }
+            }
+        }
+
+        public void AppendRow(WebMMengine.Contact contact)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row.CreateCells(dataGridView1);
+            row.Cells[0].Value = contact.NickName;
+            row.Cells[1].Value = contact.UserName;
+            dataGridView1.Rows.Add(row);
+        }
+
+
+        private void Web_on_GetContactList(System.Collections.ArrayList Contacts)
+        {
+            this.Contacts = Contacts;
+            foreach (WebMMengine.Contact x in Contacts)
+            {
+                dataGridView1.BeginInvoke(new MethodInvoker(()=>
+                {
+                    AppendRow(x);
+                }));
+            }
+
+        }
+
+        private void Web_on_scanQR()
+        {
+            Console.WriteLine("scan qr");
+        }
+
+        private void Web_on_scanQR_conform()
+        {
+            pictureBox1.BeginInvoke(new MethodInvoker(()=> {
+                pictureBox1.Hide();
+                dataGridView2.Show();
+            }));
+        }
+
+        private string chatId;
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[index];
+            String value = (String) row.Cells[0].Value;
+            label1.Text = value;
+            chatId = (String)row.Cells[1].Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string err = "";
+            web.mm_webwxsendmsg(WebMMengine.sendMsgType.文字, chatId, textBox1.Text,ref err);
+            addMsg(null, label1.Text, textBox1.Text);
+            textBox1.Text = "";
+
+        }
+
+        public void addMsg(String fromUserName,String toUserName,String content)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row.CreateCells(dataGridView2);
+            row.Cells[0].Value = fromUserName;
+            row.Cells[1].Value = toUserName;
+            row.Cells[2].Value = null;
+            row.Cells[3].Value = content;
+            dataGridView2.Rows.Add(row);
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == 13)
+            {
+                button1_Click(null, null);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
